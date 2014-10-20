56b14f783418a5bce79fdf84b58f2531db5d728c
Whitney Haddow <whitneyhaddow@gmail.com>
2014-10-20 14:19:24 -0600

    Added array to store quiz grades
    Grade calculation manipulated to include an array for an increased
    number of possible quizzes. Also included validation.

---
+++ Lab 4/Lab 4/Form1.Designer.cs
@@ -0,0 +1,273 @@
+﻿namespace Lab_4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label8 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtFinalExam = new System.Windows.Forms.TextBox();
+            this.txtMidterm = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.lblQuizDropped = new System.Windows.Forms.Label();
+            this.lblFinalLetter = new System.Windows.Forms.Label();
+            this.lblFinalGrade = new System.Windows.Forms.Label();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.lstQuizGrades = new System.Windows.Forms.ListBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.txtQuiz = new System.Windows.Forms.TextBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label8
+            // 
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(34, 29);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(357, 52);
+            this.label8.TabIndex = 19;
+            this.label8.Text = "Enter grades for each component in percentages, then press calculate to determine" +
+    " final grade.";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(34, 81);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(230, 16);
+            this.label1.TabIndex = 20;
+            this.label1.Text = "Enter up to 10 quiz grades, 1 at a time:";
+            // 
+            // txtFinalExam
+            // 
+            this.txtFinalExam.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtFinalExam.Location = new System.Drawing.Point(121, 212);
+            this.txtFinalExam.Name = "txtFinalExam";
+            this.txtFinalExam.Size = new System.Drawing.Size(179, 22);
+            this.txtFinalExam.TabIndex = 3;
+            // 
+            // txtMidterm
+            // 
+            this.txtMidterm.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMidterm.Location = new System.Drawing.Point(121, 171);
+            this.txtMidterm.Name = "txtMidterm";
+            this.txtMidterm.Size = new System.Drawing.Size(179, 22);
+            this.txtMidterm.TabIndex = 2;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(34, 218);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(77, 16);
+            this.label5.TabIndex = 22;
+            this.label5.Text = "Final Exam:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(34, 177);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(59, 16);
+            this.label4.TabIndex = 21;
+            this.label4.Text = "Midterm:";
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalculate.Location = new System.Drawing.Point(134, 268);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(151, 30);
+            this.btnCalculate.TabIndex = 4;
+            this.btnCalculate.Text = "&Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // lblQuizDropped
+            // 
+            this.lblQuizDropped.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblQuizDropped.ForeColor = System.Drawing.Color.Firebrick;
+            this.lblQuizDropped.Location = new System.Drawing.Point(37, 412);
+            this.lblQuizDropped.Name = "lblQuizDropped";
+            this.lblQuizDropped.Size = new System.Drawing.Size(356, 23);
+            this.lblQuizDropped.TabIndex = 32;
+            this.lblQuizDropped.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblFinalLetter
+            // 
+            this.lblFinalLetter.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblFinalLetter.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFinalLetter.Location = new System.Drawing.Point(214, 368);
+            this.lblFinalLetter.Name = "lblFinalLetter";
+            this.lblFinalLetter.Size = new System.Drawing.Size(179, 26);
+            this.lblFinalLetter.TabIndex = 6;
+            this.lblFinalLetter.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // lblFinalGrade
+            // 
+            this.lblFinalGrade.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblFinalGrade.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFinalGrade.Location = new System.Drawing.Point(214, 328);
+            this.lblFinalGrade.Name = "lblFinalGrade";
+            this.lblFinalGrade.Size = new System.Drawing.Size(179, 26);
+            this.lblFinalGrade.TabIndex = 5;
+            this.lblFinalGrade.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // btnExit
+            // 
+            this.btnExit.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.Location = new System.Drawing.Point(242, 438);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(151, 30);
+            this.btnExit.TabIndex = 5;
+            this.btnExit.Text = "&Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(34, 378);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(117, 16);
+            this.label7.TabIndex = 27;
+            this.label7.Text = "Final Letter Grade:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(34, 338);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(127, 16);
+            this.label6.TabIndex = 26;
+            this.label6.Text = "Final Course Grade:";
+            // 
+            // lstQuizGrades
+            // 
+            this.lstQuizGrades.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lstQuizGrades.FormattingEnabled = true;
+            this.lstQuizGrades.ItemHeight = 16;
+            this.lstQuizGrades.Location = new System.Drawing.Point(339, 84);
+            this.lstQuizGrades.Name = "lstQuizGrades";
+            this.lstQuizGrades.Size = new System.Drawing.Size(54, 164);
+            this.lstQuizGrades.TabIndex = 33;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdd.Location = new System.Drawing.Point(177, 109);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(99, 29);
+            this.btnAdd.TabIndex = 1;
+            this.btnAdd.Text = "&Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // txtQuiz
+            // 
+            this.txtQuiz.Location = new System.Drawing.Point(40, 118);
+            this.txtQuiz.Name = "txtQuiz";
+            this.txtQuiz.Size = new System.Drawing.Size(99, 20);
+            this.txtQuiz.TabIndex = 0;
+            // 
+            // btnStart
+            // 
+            this.btnStart.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStart.Location = new System.Drawing.Point(85, 438);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(151, 30);
+            this.btnStart.TabIndex = 34;
+            this.btnStart.Text = "&Start Again";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.btnAdd;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnExit;
+            this.ClientSize = new System.Drawing.Size(430, 489);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txtQuiz);
+            this.Controls.Add(this.lstQuizGrades);
+            this.Controls.Add(this.lblQuizDropped);
+            this.Controls.Add(this.lblFinalLetter);
+            this.Controls.Add(this.lblFinalGrade);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.txtFinalExam);
+            this.Controls.Add(this.txtMidterm);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label8);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Calculating Final Grade";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtFinalExam;
+        private System.Windows.Forms.TextBox txtMidterm;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Label lblQuizDropped;
+        private System.Windows.Forms.Label lblFinalLetter;
+        private System.Windows.Forms.Label lblFinalGrade;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ListBox lstQuizGrades;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.TextBox txtQuiz;
+        private System.Windows.Forms.Button btnStart;
+    }
+}
+
---
+++ Lab 4/Lab 4/Form1.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab_4
+
+/***************************************************
+ * This form calculates final grade for a student
+ * based on grades from various class components
+ * Created October 2014 by Whitney Haddow
+ ****************************************************/
+{
+    public partial class Form1 : Form
+    {
+        //declare constants
+        const decimal QUIZ_CONTRIBUTION = 0.30m;
+        const decimal MIDTERM_CONTRIBUTION = 0.30m;
+        const decimal FINAL_CONTRIBUTION = 0.40m;
+   
+        //declare an array
+        const int MAX = 10;
+        decimal[] quizzes = new decimal[MAX];
+        int count = 0; //number of quiz grades entered, 0 is default
+
+
+        public Form1()
+        {
+            InitializeComponent();
+      
+        }
+
+       //add quiz grades to the array
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            if (IsValidDecimalRange(txtQuiz, 100, "Quiz grade")) //is the input valid?
+            {
+                if (count < MAX) //more grades can still be added
+                {
+                    quizzes[count] = Decimal.Parse(txtQuiz.Text);
+                    count++; //add one more number
+                    txtQuiz.Text = ""; //clears the textbox to prepare for next entry
+                    txtQuiz.Focus(); //puts focus on the text box
+
+                    if (count == (MAX)) //last value is being added
+                    {
+                        txtMidterm.Focus(); //places focus on the midterm textbox
+                        btnAdd.Enabled = false; //they cannot click the add button anymore
+                        MessageBox.Show("The maximum amount of grades has now been entered.",
+                            "Quiz Entry Complete");
+                    }
+                }
+            }
+
+           
+
+            //display values from the array in the listbox
+            lstQuizGrades.Items.Clear();
+
+            for (int i = 0; i < count; i++)
+            {
+                lstQuizGrades.Items.Add(quizzes[i]);
+            }
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            if (IsValidDecimalRange(txtMidterm, 100, "Midterm Grade") && IsValidDecimalRange(txtFinalExam, 100, "Final Exam Grade")) //are the inputs valid?
+            {
+                
+                //declare variables
+                decimal quizAverage; //average of quiz grades remaining after lowest dropped
+                decimal midterm; //grade for midterm
+                decimal finalExam; //grade for final exam
+                decimal finalGrade; //final grade once all grades are compiled
+                char finalLetter; //final letter grade corresponding to the final grade
+                decimal quizPercent; //percent contribution of the average quiz grade
+                decimal midtermPercent; //percent contribution of midterm
+                decimal finalExamPercent; //percent contribution of final exam
+
+                //subtract lowest quiz and calculate average of remaining quiz grades
+                decimal lowestValue = FindLowest(quizzes, count);
+                decimal sum = 0.0m;
+                for (int i = 0; i < quizzes.Length; i++)
+                {
+                    sum += quizzes[i];
+                }
+                quizAverage = (sum - lowestValue) / (count - 1);
+
+                //get the percent value for quizAverage
+                decimal percentAverage = quizAverage / 100;
+
+                //get other inputs
+            
+                midterm = Convert.ToDecimal(txtMidterm.Text) / 100;
+                finalExam = Convert.ToDecimal(txtFinalExam.Text) / 100;
+
+                //calculate percent contribution for all grades
+                quizPercent = percentAverage * QUIZ_CONTRIBUTION;
+                midtermPercent = midterm * MIDTERM_CONTRIBUTION;
+                finalExamPercent = finalExam * FINAL_CONTRIBUTION;
+
+                //calculate final grade based on contributions
+                finalGrade = (quizPercent + midtermPercent + finalExamPercent);
+
+                //determine corresponding letter grade
+                if (finalGrade >= 0.9m)
+                    finalLetter = 'A';
+                else if (finalGrade >= 0.8m && finalGrade < 0.9m)
+                    finalLetter = 'B';
+                else if (finalGrade >= 0.7m && finalGrade < 0.8m)
+                    finalLetter = 'C';
+                else if (finalGrade >= 0.6m && finalGrade < 0.7m)
+                    finalLetter = 'D';
+                else
+                    finalLetter = 'F';
+
+                //display results
+                lblFinalGrade.Text = finalGrade.ToString("p0");
+                lblFinalLetter.Text = Convert.ToString(finalLetter);
+                lblQuizDropped.Text = "The quiz with grade " + lowestValue + "% was dropped!";
+
+            }
+        }
+
+
+
+
+        //allows user to start over
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            //clear textboxes and labels
+            txtMidterm.Text = "";
+            txtFinalExam.Text = "";
+            lblFinalGrade.Text = "";
+            lblFinalLetter.Text = "";
+            lblQuizDropped.Text = "";
+
+            //clear array/listbox
+            count = 0;
+            lstQuizGrades.Items.Clear();
+
+            //enable add button again
+            btnAdd.Enabled = true;
+        }
+
+        //terminate application
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+
+       //method to find the lowest quiz grade
+        public decimal FindLowest(decimal[] numbers, int howMany)
+        {
+            decimal lowest = numbers[0]; //assume first element is the lowest
+            for (int i = 0; i < howMany; i++)
+            {
+                if(numbers[i] < lowest) //if there is a lower one than the first element
+                {
+                    lowest = numbers[i];
+                }
+            }
+            return lowest;
+        }
+
+
+        //generic function to validate is a txt box a valid decimal number within specified range
+        public static bool IsValidDecimalRange(TextBox inputBox, decimal maxNumber, string inputName)
+        {
+            bool result = true; //default
+            decimal temp; //auxillary variable (temporary) for try parse
+            if (!Decimal.TryParse(inputBox.Text, out temp)) //incorrect format
+            {
+                MessageBox.Show(inputName + " should be entered as a number.",
+                    "Error");
+                inputBox.Focus();
+                result = false;
+            }
+            else if (temp < 0) //negative
+            {
+                MessageBox.Show(inputName + " must be above 0.",
+                    "Error");
+                inputBox.Focus();
+                result = false;
+            }
+            else if (temp > maxNumber) //higher than maximum
+            {
+                MessageBox.Show(inputName + " must be below " + maxNumber + ".",
+                    "Error");
+                inputBox.Focus();
+                result = false;
+            }
+            return result;
+        }
+    
+     
+    }
+      
+}
